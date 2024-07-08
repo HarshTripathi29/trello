81aa1953c875c14d17ae59e857cacc3f912bc438
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-07-08 17:56:04 +0530

    board , list and card models

--- backend/models/Board.js
+++ backend/models/Board.js
@@ -1,25 +1,27 @@
 const mongoose = require('mongoose');
 
+const cardSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    labels: [{ text: String, color: String }]
+});
+
+const listSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    cards: [cardSchema],
+});
+
 const BoardSchema = new mongoose.Schema({
     title : {
         type : String,
         required : true,
     },
-    lists : [{
-        title : {
-            type : String,
-            required : true,
-        },
-        cards : {
-            title : {
-                type : String,
-                required : true,
-            },
-            description : {
-                type : String,
-            }
-        }
-    }]
+    cover: { 
+        type: String, 
+        required: false
+    }, // Optional field for cover image
+    lists: [listSchema],
+
 })
 
 module.exports = mongoose.model('Board', BoardSchema);
